2e05404968b22f49fa15c4bf555bc07234a3ce44
Raveesha-Induwara <raveeshe.abeysinghe@gmail.com>
2025-05-20 20:41:37 +0530

    Set up React Query basics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
-import type { Metadata } from "next";
+'use client'
+// import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -12,23 +14,27 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "Whether App",
-  description: "Check the weather in your area",
-};
+// export const metadata: Metadata = {
+//   title: "Whether App",
+//   description: "Check the weather in your area",
+// };
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const queryClient = new QueryClient();
+
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <QueryClientProvider client={queryClient}>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {children}
+        </body>
+      </QueryClientProvider>
     </html>
   );
 }
